46268a32e528322537f30731d7ed031af3159ff1
Denis <denisziganshin94@gmail.com>
2015-10-23 16:22:19 +0700

    ok

---
+++ Model/Class1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class CatColor
+    {
+        public string HealthColor { get; set; }
+        public string SickColor { get; set; }
+    }
+
+    public class Cat 
+    {
+        private string _name;
+        private readonly string _age;
+        private int _health;
+        CatColor cet = new CatColor();
+        
+        public Cat(string age)
+        {
+            _age = age;
+            _health = 5;
+            cet.HealthColor = "white";
+            cet.SickColor = "green";
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (string.IsNullOrEmpty(_name))
+                    _name = value;
+            }
+        }
+
+        public string Age
+        {
+            get { return _age; }
+        }
+
+        public string Color
+        {
+            get
+            {
+                
+                if (_health==5)
+                return cet.HealthColor;
+                else
+                {
+                    return cet.SickColor;
+                }
+            }
+            set
+            {
+                
+                if (_health == 5)
+                    cet.HealthColor = value;
+                else
+                {
+                    cet.SickColor = value;
+              }
+            }
+
+        }
+
+        public void Feed()
+        {
+            if (_health!=5)
+            _health = _health + 1;
+        }
+
+        public void Punish()
+        {
+            if (_health != 0)
+                _health = _health - 1;
+        }
+
+        public string CurrentColor
+        {
+            get { return Color; }
+            set { Color = value; }
+        }
+        }
+}
---
+++ View/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+
+namespace View
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int menu = 0;
+            Console.WriteLine("Input age of cat");
+            Cat cat = new Cat(Console.ReadLine());
+
+            do
+            {
+                Console.WriteLine("\nName: {0}", cat.Name);
+                Console.WriteLine("Age: {0}", cat.Age);
+                Console.WriteLine("Current Color: {0}", cat.CurrentColor);
+                Console.WriteLine();
+                Console.WriteLine("1) Input name");
+                Console.WriteLine("2) Change color");
+                Console.WriteLine("3) Punish");
+                Console.WriteLine("4) Feed");
+                Console.WriteLine("5) Exit");
+                menu = Convert.ToInt32(Console.ReadLine());
+                switch (menu)
+                {
+                    case 1:
+                        cat.Name = Convert.ToString(Console.ReadLine());
+                        break;
+                    case 2:
+                        
+                        cat.CurrentColor = Convert.ToString(Console.ReadLine());
+                        break;
+                    case 3:
+                        cat.Punish();
+                        break;
+                    case 4:
+                        cat.Feed();
+                        break;
+                }
+            } while (menu!=5); 
+            Console.ReadKey();
+        }
+    }
+}
